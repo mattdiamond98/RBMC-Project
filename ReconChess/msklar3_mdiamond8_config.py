b7dbb2959a035858c7f92721a8dc19ab6cee572a
Matthew Sklar <msklar3@gatech.edu>
2020-11-11 14:24:38 -0500

    We are going through changes (to the state space) rn it dont move

--- ReconChess/msklar3_mdiamond8_config.py
+++ ReconChess/msklar3_mdiamond8_config.py
@@ -1,6 +1,6 @@
-EPSILON = 0.2
+EPSILON = 0.5
 MCTS_SIMULATIONS = 100
-TAU = 0.5   # Move selection exploration
+TAU = 0.8   # Move selection exploration
 LOG_EPSILON = 1e-15 # Add to probailities so you never take log 0
 LAMBDA = 0.1   # For l2-normalization overfitting via regulation prevention (TM)
 
