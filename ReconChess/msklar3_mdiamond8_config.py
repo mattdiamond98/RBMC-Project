986be07fc46efb957897485b75b22785493a52f4
Matan Diamond <mattdiamond98@gmail.com>
2020-11-18 16:41:09 -0500

    minor config changes

--- ReconChess/msklar3_mdiamond8_config.py
+++ ReconChess/msklar3_mdiamond8_config.py
@@ -6,8 +6,8 @@ LAMBDA = 0.1   # For l2-normalization overfitting via regulation prevention (TM)
 
 SIMULATION_EXPANSION = 10
 
-EPOCHS = 10
-GAMES_PER_EPOCH = 5 # Games played before training network
-TRAINING_TURNS_PER_EPOCH = 20
+EPOCHS = 5
+GAMES_PER_EPOCH = 10 # Games played before training network
+TRAINING_TURNS_PER_EPOCH = 150
 
 moves_till_loss = 200
